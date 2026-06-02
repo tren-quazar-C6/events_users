ee83c2752b05d1ad3c0463394f838e966ff45a90
FaiberCamachoDev <faibercamacho16@gmail.com>
2026-06-02 15:39:54 -0500

    fix: convert synopsis array to string in event catalog favorite button

--- resources/views/livewire/event-catalog.blade.php
+++ resources/views/livewire/event-catalog.blade.php
@@ -77,7 +77,7 @@
                                 :slug="$event['slug']"
                                 :title="$event['title']"
                                 :category="$event['category']"
-                                :synopsis="$event['synopsis'] ?? null"
+                                :synopsis="is_array($event['synopsis']) ? ($event['synopsis'][0] ?? null) : ($event['synopsis'] ?? null)"
                                 :priceFrom="$event['price_from'] ?? 0"
                                 :posterColor="$event['poster_color']"
                                 :imageUrl="$event['image_url'] ?? null"
